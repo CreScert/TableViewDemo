d7ab88884316bb2265a72e9431b4586f237da4ff
xu <876830516@163.com>
2017-09-20 15:35:57 +0800

    添加新的一种表格设计方式，随意切分。

--- app/src/main/java/com/crescert/tableviewdemo/MainActivity.java
+++ app/src/main/java/com/crescert/tableviewdemo/MainActivity.java
@@ -1,10 +1,25 @@
 package com.crescert.tableviewdemo;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.crescert.tableview.TableView;
+import com.crescert.tableview.TableView2;
+import com.crescert.tableview.TableViewList;
+import com.crescert.tableview.TableViewModel;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,24 +28,109 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TableView layout = (TableView) findViewById(R.id.gridLayout);
-        layout.setWidthTableCount(5).setHeightTableCount(2048);
+//        TableView2 layout = (TableView2) findViewById(R.id.gridLayout);
+//        layout.setWidthTableCount(5).setHeightTableCount(2048);
+//
+//        ImageView imageView = new ImageView(this);
+//        imageView.setImageResource(R.mipmap.ic_launcher);
+//        layout.setView(imageView,2,2);
+//
+//        ImageView imageView1 = new ImageView(this);
+//        imageView1.setImageResource(R.mipmap.ic_launcher_round);
+//        layout.setView(imageView1,4,4);
+//
+//        ImageView imageView2 = new ImageView(this);
+//        imageView2.setImageResource(R.mipmap.ic_launcher_round);
+//        layout.setView(imageView2,3,3);
+//        ImageView imageView3 = new ImageView(this);
+//        imageView3.setImageResource(R.mipmap.ic_launcher_round);
+//        layout.setView(imageView3,1,1);
 
-        ImageView imageView = new ImageView(this);
-        imageView.setImageResource(R.mipmap.ic_launcher);
-        layout.setView(imageView,2,2);
+        try {
+            //读取资产文件
+            String s = parseJson("123.txt");
+            //解析JSON
+            Gson gson = new Gson();
+            TableViewList tableViewModel = gson.fromJson(s, TableViewList.class);
+            //为放入表格做最后一步处理，第二个参数是左右边界
+            parseTable(tableViewModel.gridlist,tableViewModel.padding);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        ImageView imageView1 = new ImageView(this);
-        imageView1.setImageResource(R.mipmap.ic_launcher_round);
-        layout.setView(imageView1,4,4);
+    /**
+     * 解析
+     * @param gridlist
+     * @param padding
+     */
+    private void parseTable( List<TableViewModel> gridlist, int padding) {
+        TableView table = (TableView) findViewById(R.id.gridList);
 
-        ImageView imageView2 = new ImageView(this);
-        imageView2.setImageResource(R.mipmap.ic_launcher_round);
-        layout.setView(imageView2,3,3);
-        ImageView imageView3 = new ImageView(this);
-        imageView3.setImageResource(R.mipmap.ic_launcher_round);
-        layout.setView(imageView3,1,1);
+        //新建一个临时集合，因为要修改源集合的值，
+        List<TableViewModel> CopyJson = new ArrayList<>(gridlist.size());
+
+        //拷贝源集合到新集合中
+        for (int i  = 0;  i < gridlist.size(); i++) {
+            CopyJson.add(gridlist.get(i));
+        }
+
+        //适配
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
 
 
+        //适配
+        double ratio = (width * 1.0f) / 750;
+
+        //总高度
+        int totalHeight = 0;
+
+        //将集合中所有的高度修改为适配高度
+        for (int i = 0; i < CopyJson.size(); i++) {
+            //每修改一处，设置标记，否则会一直计算下去
+            if(!CopyJson.get(i).isCalc) {
+                CopyJson.get(i).height *= ratio;
+                //修改标记
+                CopyJson.get(i).isCalc = true;
+            }
+        }
+
+        //将所有的高度加起来，其实可以和上面的循环连起来
+        for (int i = 0; i < CopyJson.size(); i++) {
+            totalHeight += CopyJson.get(i).height;
+        }
+
+        //设置组件的高度
+        ViewGroup.LayoutParams params2 = table.getLayoutParams();
+        params2.height = totalHeight;
+        table.setLayoutParams(params2);
+        //设置组件的左右边距
+        table.setPadding((int) (padding*ratio),0, (int) (padding*ratio),0);
+
+        //开始绘制组件
+        table.initJson(CopyJson, ImageView.ScaleType.FIT_XY);
+    }
+
+    /**
+     * 读取文件
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
+    public String parseJson(String filePath) throws IOException {
+        InputStream open = getResources().getAssets().open(filePath);
+        byte[] bytes = new byte[100];
+        int len = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ((len = open.read(bytes)) != -1) {
+            stringBuilder.append(new String(bytes, 0, len));
+        }
+        Log.e("sdf", stringBuilder.toString() + "");
+        return stringBuilder.toString();
     }
 }
--- tableview/src/main/java/com/crescert/tableview/TableView.java
+++ tableview/src/main/java/com/crescert/tableview/TableView.java
@@ -6,32 +6,29 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.util.HashMap;
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * Created by 续杰 on 2017/8/11.
+ * Created by CreScert on 2017/8/11.
  */
 
 public class TableView extends ViewGroup {
 
-    //屏幕的宽和高
-    private int mScreenWidth = 0;
-    private int mScreenHeight = 0;
+    private List<TableViewModel> json;      //原始数据
+    private List<TableViewModel> CopyJson;  //拷贝数据
+    private float mScreenWidth;               //总宽度
+    private float mScreenHeight;              //总高度
 
-    //总共的格子数
-    private int mWidthTable = 8;//横向
-    private int mHeightTable = 80;//纵向
-
-    //每一个格子占的像素
-    private int mPerPiexl = 0;
-    //是否 通过View大小判断所需要的格子内部有没有被占用
-    private boolean mIsFilterTable = true;
-
-    /**
-     * 格子占用情况
-     */
-    private int position[][] = null;
+    //图片缩放方式int
+    ImageView.ScaleType mScaleType = ImageView.ScaleType.FIT_XY;
 
     public TableView(Context context) {
         this(context, null);
@@ -44,62 +41,133 @@ public class TableView extends ViewGroup {
     public TableView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initPixel();
+    }
+
+
+    /**
+     * 解析bean类并绘制
+     *
+     * @param json
+     */
+    public void initJson(List<TableViewModel> json) {
+        initJson(json, mScaleType);
+    }
+
+    /**
+     * 解析bean类绘制组件但是不刷新
+     *
+     * @param json
+     * @param scaleType 图片的缩放方式
+     */
+    public void initJsonNoFrush(List<TableViewModel> json, ImageView.ScaleType scaleType) {
+        //保存值
+        this.json = json;
+        mScaleType = scaleType;
+
+        //临时变量
+        CopyJson = new ArrayList<>(json.size());
+        //填充数据
+        for (int i = 0; i < json.size(); i++) {
+            CopyJson.add(null);
+        }
+        //拷贝原数据
+        Collections.copy(CopyJson, json);
+
+        //总高度
+        float mHeight = 0;
+        //高度累积
+        for (int i = 0; i < json.size(); i++) {
+            mHeight += json.get(i).height;
+        }
+//        mHeight = json.height;
+        //测量控件
+        measure((int) mScreenWidth, (int) mHeight);
 
     }
 
     /**
-     * 初始化每个格子的像素大小，以及格子的分配
+     * 解析bean类并绘制，设置图片的缩放方式
+     *
+     * @param json
+     * @param scaleType 图片的缩放方式
      */
-    public void initPixel() {
-        //保存屏幕的宽和高
+    public void initJson(List<TableViewModel> json, ImageView.ScaleType scaleType) {
+        //解析bean类
+        initJsonNoFrush(json, scaleType);
+        //绘制
+        requestLayout();
+    }
+
+    /**
+     * 初始化像素值
+     */
+    private void initPixel() {
+        //获取屏幕的宽和高
         DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
         mScreenWidth = displayMetrics.widthPixels;
         mScreenHeight = displayMetrics.heightPixels;
-        mPerPiexl = mScreenWidth / mWidthTable;
-
-        position = new int[mHeightTable][mWidthTable];
-    }
-
-    /**
-     * 设置横向X轴的格子数量
-     * @param widthTable
-     * @return
-     */
-    public TableView setWidthTableCount(int widthTable) {
-        mWidthTable = widthTable;
-        initPixel();
-        return this;
-    }
-
-    /**
-     * 设置纵向Y轴的格子数量
-     * @param heightTable
-     * @return
-     */
-    public TableView setHeightTableCount(int heightTable) {
-
-        if(heightTable <= 2048) {
-            mHeightTable = heightTable;
-            initPixel();
-        }
-        return this;
     }
 
 
     /**
-     * 判断View大小区域内各自有没有被占用
+     * 设置图片的显示方式
      *
-     * @param isFilterTable
-     * @return
+     * @param scaleType
      */
-    public TableView setFilterTable(boolean isFilterTable) {
-        mIsFilterTable = isFilterTable;
-        return this;
+    public void setScaleType(ImageView.ScaleType scaleType) {
+        mScaleType = scaleType;
+        //请求父控件刷新
+        requestLayout();
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    /**
+     * 设置宽度
+     *
+     * @param width
+     */
+    public void setWidth(float width) {
+        //设置宽度
+        LayoutParams layoutParams = getLayoutParams();
+        if (null != layoutParams) {
+            getLayoutParams().width = (int) width;
+        }
+        //保存宽度
+        mScreenWidth = width;
+        requestLayout();
+    }
+
+    /**
+     * 转换权值，将高度
+     *
+     * @param imagesInfo
+     * @param width
+     * @param height
+     * @param orientation
+     */
+    public void converWeight(List<TableViewModel.SubitemsInfo> imagesInfo, float width, float height, String orientation) {
+        ArrayList<Float> integers = new ArrayList<>();
+        //总权值
+        float totalWeight = 0;
+
+        //相加所有的权重
+        for (int i = 0; i < imagesInfo.size(); i++) {
+            //获取到各个权重
+            integers.add( imagesInfo.get(i).weight);
+            //累积权重
+            totalWeight += imagesInfo.get(i).weight;
+        }
+
+        float calc = 0;
+        //如果是水平的
+        if (orientation.equals("h")) {
+            calc = width;
+        } else {
+            calc = height;
+        }
+        //以屏幕大小转换权重
+        for (int i = 0; i < integers.size(); i++) {
+            imagesInfo.get(i).weight = calc * integers.get(i) / totalWeight;
+        }
     }
 
     /**
@@ -113,162 +181,199 @@ public class TableView extends ViewGroup {
      */
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        //获取有多少个子控件
-        int childCount = getChildCount();
-        //根据子控件数量设置
-        for (int i = 0; i < childCount; i++) {
-            //如果说第一次进来，或者再次调用这个方法时重置所有坐标
-            if (i == 0) {
-                for (int j = 0; j < position.length; j++) {
-                    for (int k = 0; k < position[j].length; k++) {
-                        position[j][k] = 0;
-                    }
+
+        if (null != json) {
+            //移除所有的控件
+            removeAllViews();
+            //初始化坐标
+            initPosition();
+
+            //最外层遍历
+            for (int i = 0; i < json.size(); i++) {
+                //每次进来都从屏幕左边开始
+                startX = 0;
+                //第一次进来，从左上角开始
+                if (i == 0) {
+                    startY = 0;
                 }
+
+                //中间这层
+                TableViewModel imagesInfo = json.get(i);
+                if (null != imagesInfo) {
+                    //最里层
+                    layoutView(imagesInfo.subitems, mScreenWidth,  imagesInfo.height, imagesInfo.orientation, imagesInfo);
+                }
+                //高度加保存的Y值，作为新的Y值
+                startY = (imagesInfo.height + TotalStartY);
+                //并保存
+                TotalStartY = startY;
             }
-            //获取每一个子控件
-            View view = getChildAt(i);
-            //获取子控件的坐标
-            HashMap<Integer, Integer> posValue = hash.get(view);
 
-            //为Null判断
-            if (posValue != null) {
-                //获取子控件将要设置的大小
-                for (Integer needWidthCount : posValue.keySet()) {
-                    //获取需要的y轴方格子的数量
-                    Integer needHeightCount = posValue.get(needWidthCount);
+        }
+    }
 
-                    //寻找有空的方格子
-                    Position empty = findEmpty(needWidthCount, needHeightCount);
-                    //找到了
-                    if (empty != null) {
-                        //获取x,y轴的方格子坐标
-                        int x = empty.x;
-                        int y = empty.y;
-                        //绘制布局
-                        view.layout(x * mPerPiexl, y * mPerPiexl, needWidthCount * mPerPiexl + x * mPerPiexl, y * mPerPiexl + needHeightCount * mPerPiexl);
+    /**
+     * 初始化坐标以及数据
+     */
+    private void initPosition() {
+        //初始化坐标
+        startX = 0;
+        startY = 0;
 
-                        //填充方格子
-                        fillTable(x, y, needWidthCount, needHeightCount);
+        TotalStartY = 0;
+        //我为啥要拷贝？
+        Collections.copy(json, CopyJson);
+        initJsonNoFrush(json, mScaleType);
+    }
+
+
+    public float startX = 0;
+    public float startY = 0;
+    //随时保存Y值
+    public float TotalStartY = 0;
+
+    /**
+     * 循环去查找，如果没有孩子，就显示当前的布局
+     * @param imagesInfo    子控件的数据
+     * @param width         子控件所拥有的宽度
+     * @param height        子控件所拥有的高度
+     * @param orientation   排序方式
+     * @param fatherInfo    最上层的数据，父类，保持不变的往下传
+     */
+    private void layoutView(final List<TableViewModel.SubitemsInfo> imagesInfo, float width, float height, String orientation, TableViewModel fatherInfo) {
+        //转换下权重
+        converWeight(imagesInfo, width, height, orientation);
+
+        //每个子类拥有的总宽和总长
+        float childHeight = height;
+        float childWidth = width;
+
+        //子类所减小剩余的宽度和高度
+        float haveHeight = childHeight;
+        float haveWidth = childWidth;
+
+        //之前的X和Y
+        float startX_Old = startX;
+        float startY_Old = startY;
+
+        if (null != imagesInfo) {
+            //总权重值
+            int weight = 0;
+            //获取总权重值
+            for (int i = 0; i < imagesInfo.size(); i++) {
+                weight += imagesInfo.get(i).weight;
+            }
+            int haveWeight = weight;
+
+            //遍历查找孩子
+            for (int i = 0; i < imagesInfo.size(); i++) {
+                //如果有孩子，继续向下寻找
+                if (null != imagesInfo.get(i).subitems) {
+                    //垂直
+                    if (imagesInfo.get(i).orientation.equals("v"))
+                        layoutView(imagesInfo.get(i).subitems,/* haveWidth / haveWeight **/  imagesInfo.get(i).weight, haveHeight, imagesInfo.get(i).orientation, fatherInfo);
+                    else//水平
+                        layoutView(imagesInfo.get(i).subitems, haveWidth, /*haveHeight **/  imagesInfo.get(i).weight/*/ haveWeight*/, imagesInfo.get(i).orientation, fatherInfo);
+                } else {  //没有孩子，就显示控件
+                    //创建图片
+                    ImageView imageView = new ImageView(getContext());
+
+                    //测量下
+                    imageView.measure(0, 0);
+                    //设置图片缩放方式
+                    imageView.setScaleType(mScaleType);
+
+                    //点击事件
+                    final int finalI = i;
+                    imageView.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(getContext(), "点击了图片", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    //图片展示
+//                    Glide.loadHomePage(getContext(), AccountClient.QINIUPIC + imagesInfo.get(i).img, imageView);
+                    Log.e("aaaa",imagesInfo.get(i).img);
+                    Picasso.with(getContext()).load(imagesInfo.get(i).img)
+                            .error(R.mipmap.ic_launcher_round)
+                            .placeholder(R.mipmap.ic_launcher).into(imageView);
+                    //水平方向，从左往右
+                    if (orientation.equals("h")) {
+
+                        //右下角的x轴，也是这个控件所占用的宽度
+                        //现在所在的X轴点坐标+ 占用的大小（当前控件所拥有的宽度/总权重*当前对象控件的权重（每一份））
+                        float fitWidth = startX + childWidth / weight * imagesInfo.get(i).weight;
+
+                        //布局当前控件
+                        imageView.layout((int)startX, (int)startY, (int)fitWidth, (int)(startY + childHeight));
+
+                        //当前控件的右下角作为现在的X轴
+                        startX = fitWidth;
+
+                        //计算剩余的宽度（当前控件所拥有的宽度-占用宽度后的X轴）
+                        haveWidth = Math.abs(childWidth - startX);
+
+                        //如果是当前行最后一个
+                        if (imagesInfo.size() - 1 == i) {
+                            //如果当前父类拥有高度+之前保存的高度值-现在的高度-所占有的高度小于5，
+                            // 说明要换到下一行，只是绘制一小部分，要换行了
+                            //x轴还原
+                            if (!(Math.abs(fatherInfo.height + TotalStartY - startY - childHeight) < 5)) {
+                                //高度累积
+                                startY += childHeight;
+                                //X轴还原
+                                startX = startX_Old;
+                            } else {
+                                //换到右边
+                                //高度还原
+                                startY = TotalStartY;
+                                //X轴到控件的右下角
+                                startX = fitWidth;
+                            }
+                        }
 
                     } else {
-                        Log.e("没有位置了", needWidthCount + "..." + needHeightCount);
-                    }
-                }
-            }
-        }
+                        //从上到下
 
-    }
+                        //占用的高度，同上，右下角Y轴
+                        float fitHeight = startY + childHeight / weight * imagesInfo.get(i).weight;
 
-    /**
-     * 填充方格子，从之前找到的有位置的坐标开始，填充需要的方格子数量（图片大小）
-     *
-     * @param x
-     * @param y
-     * @param needWidthCount
-     * @param needHeightCount
-     */
-    private void fillTable(int x, int y, Integer needWidthCount, Integer needHeightCount) {
-        //填充方格子
-        for (int j = y; j < needWidthCount + y; j++) {
-            for (int k = x; k < needHeightCount + x; k++) {
-                //防止坐标越界
-                if (k == mWidthTable) break;
-                position[j][k] = 1;
-            }
-        }
-    }
+                        //绘制控件，X轴要用之前的X轴累积
+                        imageView.layout((int)startX,(int) startY, (int)(startX + childWidth), (int)fitHeight);
 
-    /**
-     * 保存所占有的方格子的位置
-     */
-    class Position {
-        int x;
-        int y;
+                        //占用的高度，代替现在的Y轴
+                        startY = fitHeight;
+                        //当前控件剩余的高度（当前控件拥有的高度-占有的高度+之前的Y轴）
+                        haveHeight = Math.abs(childHeight - startY + TotalStartY);
 
-        public Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    /**
-     * 寻找新的方格子，
-     *
-     * @param integer
-     * @param integer1
-     * @return
-     */
-    private Position findEmpty(Integer integer, Integer integer1) {
-
-        for (int i = 0; i < position.length; i++) {
-            for (int j = 0; j < position[i].length; j++) {
-                if (position[i][j] != 1) {
-                    //判断在图片大小的位置上是否有占用的方格子，
-                    if (mIsFilterTable) {
-                        //如果有，继续寻找
-                        if (isFill(i, j, integer, integer1)) {
-                            continue;
+                        //最后一列
+                        if (imagesInfo.size() - 1 == i) {
+                            //X轴累加
+                            startX += childWidth;
+                            //Y轴还原
+                            startY = startY_Old;
                         }
                     }
-                    //有足够的位置返回坐标
-                    if (mWidthTable - integer > 0) {
-                        return new Position(j, i);
-                    }
+                    //剩余的高度，减去现在的权重（当前json是权重为高度），没用到
+                    haveWeight -= imagesInfo.get(i).weight;
+
+                    //给父控件添加子控件
+                    addView(imageView);
                 }
             }
         }
-        return null;
     }
 
-    /**
-     * 判断在图片的位置是否有填充过
-     *
-     * @param i
-     * @param j
-     * @param integer
-     * @param integer1
-     * @return
-     */
-    private boolean isFill(int i, int j, Integer integer, Integer integer1) {
-        for (int k = i; k < integer + i; k++) {
-            for (int l = j; l < integer1 + j; l++) {
-                //如果图片的大小超过了边界就继续寻找
-                if (l == mWidthTable)
-                    return true;
-                //代表已经被占用
-                if (position[k][l] == 1) {
-                    return true;
-                }
 
+    private float getWeight(List<TableViewModel.SubitemsInfo> imagesInfo) {
+        float weight = 0;
+        if (null != imagesInfo && 0 < imagesInfo.size()) {
+            for (int i = 0; i < imagesInfo.size(); i++) {
+                weight += imagesInfo.get(i).weight;
+                weight += getWeight(imagesInfo.get(i).subitems);
             }
         }
-        return false;
-    }
-
-
-    /**
-     * 保存View以及所需空间
-     */
-    private HashMap<View, HashMap<Integer, Integer>> hash = new HashMap<>();
-
-    /**
-     * 设置View
-     *
-     * @param view            指定的View
-     * @param needWidthTable  需要的x轴方格子的数量
-     * @param needHeightTable 需要的y轴方格子的数量
-     */
-    public void setView(View view, int needWidthTable, int needHeightTable) {
-        if (needWidthTable > mWidthTable) {
-            throw new ArrayIndexOutOfBoundsException("格子的大小超过原有设定");
-        }
-        if (null == view) {
-            return;
-        }
-        HashMap<Integer, Integer> integerIntegerHashMap = new HashMap<>();
-        integerIntegerHashMap.put(needWidthTable, needHeightTable);
-
-        hash.put(view, integerIntegerHashMap);
-        addView(view);
+        return weight;
     }
 }
+
